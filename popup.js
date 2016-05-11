4c76f5e68b61981d3f50066aafee9febae8e6ce7
kingbird <4739345@qq.com>
2016-05-11 23:37:55 +0800

    1

--- popup.js
+++ popup.js
@@ -1,9 +1,13 @@
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.cookies.get({url: tabs[0].url, name: 'ptui_loginuin'}, function(cookie) {
-        var cookie = cookie.value || 'hi';
-        chrome.tabs.sendMessage(tabs[0].id, {greeting: cookie}, function(response) {
+    // chrome.cookies.get({url: tabs[0].url, name: 'ptui_loginuin'}, function(cookie) {
+    //     var cookie = cookie.value || 'hi';
+    //     chrome.tabs.sendMessage(tabs[0].id, {greeting: cookie}, function(response) {
+    //         console.log(response);
+    //     });
+    // });
+    chrome.cookies.set({url: tabs[0].url, name: 'version', value: 'trunk'}, function(ret) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: ret}, function(response) {
             console.log(response);
         });
     });
-
 });
